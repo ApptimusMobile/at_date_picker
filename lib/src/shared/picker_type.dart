d63947f22ad2f942218a8712a4ae30475c63ca6e
umakaran uma <umakaranuma1126@gmail.com~>
2024-04-30 15:52:17 +0530

    at_date_picker

---
+++ lib/src/shared/picker_type.dart
@@ -0,0 +1,26 @@
+import 'package:at_date_picker/at_date_picker.dart';
+
+/// Initial display of the picker.
+///
+/// See also:
+///
+///  * [showRangePickerDialog], which shows a dialog that
+///     contains a range picker.
+///
+///  * [RangeDatePicker], widget which implements a Range picker.
+///
+///  * [showDatePickerDialog], which shows a dialog that
+///     contains a date picker.
+///
+///  * [DatePicker], widget which implements a date picker.
+///
+enum PickerType {
+  /// Choosing a day.
+  days,
+
+  /// Choosing a month.
+  months,
+
+  /// Choosing a year.
+  years,
+}
